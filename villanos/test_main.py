293f9fb5ba17d46ed9c67975c38748f1afffed59
beeva-manuelgarcia <manuel.garcia@beeva.com>
2015-10-05 23:16:17 +0200

    add test

---
+++ villanos/test_main.py
@@ -0,0 +1,23 @@
+from unittest import TestCase
+from main import pintarVillanos, esVillano, generarVillanos2
+__author__ = 'manuel'
+
+
+class TestMain(TestCase):
+#  def test_main(self):
+#    self.fail()
+
+  def test_pintar_villanos(self):
+    s=pintarVillanos
+
+    self.assertEqual(s([[]]), None)
+
+  def test_generar_villanos2(self):
+    s=generarVillanos2
+
+    self.assertEqual(len(s(1)), 1)
+
+  def test_es_villano(self):
+    s=esVillano
+    v1=['A', 10, 1000, 10, 1]
+    self.assertEqual(s(v1), True)
