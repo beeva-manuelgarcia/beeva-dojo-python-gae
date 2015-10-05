ce17720f48bb3a9aa89f906105723ca3c371003f
beeva-manuelgarcia <manuel.garcia@beeva.com>
2015-10-05 22:54:05 +0200

    Revision

--- villanos/main.py
+++ villanos/main.py
@@ -1,18 +1,34 @@
 __author__ = 'manuel'
 from random import randint
 
-DIAS=int(7)
-HORAS=int(24)
-VILLANOMAX=int(3)
+DIAS = int(7)
+HORAS = int(24)
+VILLANOMAX = int(3)
+SEMANA = ['Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes', 'Sabados', 'Domingo']
+
+
+
+def generarVillanos2(N):
+    L = [ ]
+    for i in range(N):
+        L.append(['Villano'+str(i), randint(1,30), randint(1000,3000), randint(1,10), randint(0,1)])
+    return L
+
 
 def generarVillanos():
 	#Nombre, vida, recompensa, distancia, villano(1),
-	v1=['A', randint(1,30), randint(1000,3000),randint(1,10),randint(0,1)]
-	v2=['B', randint(1,30), randint(1000,3000),randint(1,10),randint(0,1)]
-	v3=['C', randint(1,30), randint(1000,3000),randint(1,10),randint(0,1)]
-
+	v1=['A', randint(1,30), randint(1000,3000), randint(1,10), randint(0,1)]
+	v2=['B', randint(1,30), randint(1000,3000), randint(1,10), randint(0,1)]
+	v3=['C', randint(1,30), randint(1000,3000), randint(1,10), randint(0,1)]
 	matriz=[v1,v2,v3]
 	return matriz
+
+
+def pintarVillanos(villanos):
+    print '[Nombre, Vida, Recompensa, Distancia, Villano]'
+    for i in range(len(villanos)):
+        print villanos[i]
+
 
 def ordenarVillanos(villanos):
 	villanosOrd=villanos
@@ -20,28 +36,44 @@ def ordenarVillanos(villanos):
 	return villanosOrd
 
 
-villanos=generarVillanos()
-print 'Matriz M = ',villanos
-
-#ordenamos villanos por distancia para ir a por ellos
-print ordenarVillanos(villanos)
-
-aux=ordenarVillanos(villanos)
-villano=0
-vida=1
-horatotales=0
-for i in range(DIAS):
-    print 'Dia: ',int(i+1)
-    for j in range(HORAS):
-		print 'Hora: ', j, 'Villano: ', aux[villano][0], 'Vida: ', aux[villano][1]
-		aux[villano][1]=aux[villano][1]-1
-		if aux[villano][1]<=0:
-			villano += 1
-			if villano>=VILLANOMAX:
-				break
-    if villano>=VILLANOMAX:
-        break
+def esVillano(villano):
+    if int(villano[4])==int(1):
+        return True
+    else:
+        return False
 
 
+def main():
+    villanos = generarVillanos2(randint(5,10))
+    pintarVillanos(villanos)
 
+    #ordenamos villanos por distancia para ir a por ellos
+    villanosOrd=ordenarVillanos(villanos)
+    #print villanosOrd
 
+    villanoNum = 0
+    horasTotales = int(0)
+    gananciasTotales = int(0)
+    for i in range(DIAS):
+        print 'Dia: ',int(i+1),' ', SEMANA[i]
+        for j in range(HORAS):
+            if esVillano(villanosOrd[villanoNum]):
+                horasTotales+=1
+                print 'Hora: ', j, 'Villano: ', villanosOrd[villanoNum][0], 'Vida: ', villanosOrd[villanoNum][1]
+                villanosOrd[villanoNum][1] -= 1
+                if villanosOrd[villanoNum][1]<=0:
+                    gananciasTotales+=villanosOrd[villanoNum][2]
+                    villanoNum += 1
+                    if villanoNum>=len(villanosOrd):
+                        break
+            else:
+                villanoNum += 1
+                if villanoNum>=len(villanosOrd):
+                    break
+        if villanoNum>=len(villanosOrd):
+            break
+    print 'Horas totales trabajadas: ', horasTotales
+    print 'Ganancias totales: ', gananciasTotales
+
+if __name__ == '__main__':
+    main()
